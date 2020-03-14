ab6132559c3280e0b8ebe4531b0cd75b895dc7a6
Isaac Smith <isaacsmith215@aol.com>
2020-03-14 19:00:50 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,13 @@
+def sort_array_asc(array)
+  asc = array.sort do |b, a|
+    b <=> a
+  end
+  return asc
+end
 
+def sort_array_desc(array)
+  des = array.sort do |a,b|
+    b <=> a
+  end
+  return des
+end
